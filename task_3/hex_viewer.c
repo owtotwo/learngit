ed0ea29029f1dea8f05eb44a4275ddf0b469ba79
owtotwo <owtotwo@163.com>
2016-09-14 12:07:52 +0800

    The right answer

--- task_3/hex_viewer.c
+++ task_3/hex_viewer.c
@@ -1,39 +1,79 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <assert.h>
+// Copyright (c) 2016 sysu_AT
+// A hexdump(Unix) reimplementation for task 3
+
+#include <stdio.h> // for fopen, ferror, feof, fclose, fgetc
+#include <assert.h> // for assert
+#include <ctype.h> // for isprint
+#include <stdlib.h> // for atoi
+
+#define ROW_BYTE 16
+#define LINE_HEAD_SIZE_DEFAULT 8
+
+const char CONTROL_CHAR_DISPLAY = '.';
+int line_head_size = 8;
 
 // Considers fp has been opened successfully.
-print_file_stream_in_hex(FILE* fp) {
-	assert(fp);
-	
+void print_file_stream_in_hex(FILE* fp) {
+	assert(!ferror(fp));
+
+	size_t pos = 0;
+	char line[ROW_BYTE] = {0};
+
+	for (pos = 0; !feof(fp); pos++) {
+		if (pos % 16 == 0) printf("%0*x  ", line_head_size, (unsigned int)pos);
+		
+		char c = fgetc(fp);
+
+		if (feof(fp)) {
+			size_t extra_byte = pos % ROW_BYTE;
+			// padding the last line by whitespace
+			size_t padding_size = (ROW_BYTE - extra_byte) * 3;
+			// add a whitespace for every 8 byte.
+			padding_size += (((pos % 16) / 8) == 0 ? 2 : 1);
+			for (int i = 0; i < extra_byte; i++) {
+				if (i == 0) printf("%*s|", (unsigned int)padding_size, "");
+				putchar(isprint(line[i]) ? line[i] : CONTROL_CHAR_DISPLAY);
+				if (i == extra_byte - 1) printf("|\n");
+			}
+		} else {
+			printf("%02x ", (unsigned char)c);
+			// add a whitespace for every 8 byte.
+			if (pos % 8 == 7) putchar(' ');
+			// save the printable char
+			line[pos % ROW_BYTE] = c;
+			// line tail
+			if (pos % 16 == 15) {
+				// display the char
+				for (int i = 0; i < ROW_BYTE; i++) {
+					if (i == 0) printf("|");
+					putchar(isprint(line[i]) ? line[i] : CONTROL_CHAR_DISPLAY);
+					if (i == ROW_BYTE - 1) printf("|");
+				}
+				putchar('\n');
+			}
+		}
+	}
+	if (--pos % 16 != 0) printf("%0*x", line_head_size, (unsigned int)pos);
+	putchar('\n');
 }
+
 
 int main(int argc, char* argv[]) {
 	
-	if (argc < 2) {
+	if (argc < 2 || argc > 3) {
 		printf("Usage: ./hex_viewer [file]\n");
-		exit(1);
+		return -1;
 	}
+
+	line_head_size = argc == 3 ? atoi(argv[2]) : LINE_HEAD_SIZE_DEFAULT; // init
 	
 	FILE* fp = fopen(argv[1], "rb");
 	if (!fp) {
 		printf("Cannot open the file %s.\n", argv[1]);
-		exit(1);
+		return -1;
 	}
 
-	fseek(fp, 0, SEEK_END);
-	int length = ftell(fp);
-	fseek(fp, 0, SEEK_SET);
-
-	char* buffer = (char*)malloc(length);
-	fread(buffer, sizeof(char), length, fp);
-
-	for (int i = 0; i < length; i++) {
-		if (i % 16 == 0) printf("%08x| ", i);
-		printf("%02x ", (unsigned char)buffer[i]);
-		if (i % 16 == 15) putchar('\n');
-	}
-	putchar('\n');
+	print_file_stream_in_hex(fp);
 
 	fclose(fp);
 	return 0;
