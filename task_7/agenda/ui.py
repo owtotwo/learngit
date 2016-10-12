d6eff8339ec29dc103cf1b0907a6d351952de154
owtotwo <owtotwo@163.com>
2016-10-12 13:15:47 +0800

    done

--- task_7/agenda/ui.py
+++ task_7/agenda/ui.py
@@ -2,62 +2,61 @@ class UI(object):
     """The _agendaUI Class in _agenda."""
     def __init__(self):
         pass
-    
-    @staticmethod
-    def run():
-        print "Running..."
 
-def start_agenda(self):
-    pass
-    
-def get_operation(self):
-    pass
-    
-def execute_operation(self):
-    pass
-    
-def user_log_in(self):
-    pass
-    
-def user_register(self):
-    pass
-    
-def user_log_out(self):
-    pass
-    
-def quit_agenda(self):
-    pass
-    
-def delete_user(self):
-    pass
-    
-def list_all_users(self):
-    pass
-    
-def create_meeting(self):
-    pass
-    
-def list_all_meetings(self):
-    pass
-    
-def list_all_sponsor_meetings(self):
-    pass
-    
-def list_all_participate_meetings(self):
-    pass
-    
-def query_meeting_by_title(self):
-    pass
-    
-def query_meeting_by_time_interval(self):
-    pass
-    
-def delete_meeting_by_title(self):
-    pass
-    
-def delete_all_meetings(self):
-    pass
-    
-def print_meetings(self):
-    pass
+    def operation_loop(self):
+        print "Running Operation Loop..."
+
+    def _start_agenda(self):
+        pass
+        
+    def _get_operation(self):
+        pass
+        
+    def _execute_operation(self):
+        pass
+        
+    def _user_log_in(self):
+        pass
+        
+    def _user_register(self):
+        pass
+        
+    def _user_log_out(self):
+        pass
+        
+    def _quit_agenda(self):
+        pass
+        
+    def _delete_user(self):
+        pass
+        
+    def _list_all_users(self):
+        pass
+        
+    def _create_meeting(self):
+        pass
+        
+    def _list_all_meetings(self):
+        pass
+        
+    def _list_all_sponsor_meetings(self):
+        pass
+        
+    def _list_all_participate_meetings(self):
+        pass
+        
+    def _query_meeting_by_title(self):
+        pass
+        
+    def _query_meeting_by_time_interval(self):
+        pass
+        
+    def _delete_meeting_by_title(self):
+        pass
+        
+    def _delete_all_meetings(self):
+        pass
+        
+    def _print_meetings(self):
+        pass
     
