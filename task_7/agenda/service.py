07c190a60d0192f106a404fb56b40636286a66d8
owtotwo <owtotwo@163.com>
2016-10-12 14:42:34 +0800

    service param done

--- task_7/agenda/service.py
+++ task_7/agenda/service.py
@@ -1,43 +1,47 @@
 from storage import storage 
 class Service(object):
     """The AgendaService Class in Agenda."""
-    def __init__(self):
-        self.storage = 
+    def __init__(self, _storage):
+        self._storage = _storage
     
-    def user_log_in(self):
+    def user_log_in(self, username, password):
         pass
 
-    def user_register(self):
+    def user_register(self, username, password, email, phone):
         pass
         
-    def user_register(self):
+    def delete_user(self, username, password):
         pass
         
     def list_all_users(self):
         pass
         
-    def create_meeting(self):
+    def create_meeting(self, username, title, start_date, end_date,
+            participator):
         pass
         
-    def meeting_query(self):
+    def meeting_query(self, username, title=None,
+            start_date=None, end_date=None):
+        if title is not None:
+            pass
+        elif start_date is not None and end_date is not None:
+            pass
+        else:
+            raise ValueError()
+        
+    def list_all_meetings(self, username):
         pass
         
-    def meeting_query(self):
+    def list_all_sponsor_meetings(self, username):
         pass
         
-    def list_all_meetings(self):
+    def list_all_participate_meetings(self, username):
         pass
         
-    def list_all_sponsor_meetings(self):
+    def delete_meeting(self, username, title):
         pass
         
-    def list_all_participate_meetings(self):
-        pass
-        
-    def delete_meeting(self):
-        pass
-        
-    def delete_all_meetings(self):
+    def delete_all_meetings(self, username):
         pass
         
     def start_agenda(self):
